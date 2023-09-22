17ddf950ac12a843a3f8f6650cd7cdf4d95c2cac
Carlos Dubón <61991110+celtaced@users.noreply.github.com>
2023-09-22 15:27:45 -0600

    Create M02 - ScriptEjercicios.sql

---
+++ M02 - ScriptEjercicios.sql
@@ -0,0 +1,7 @@
+-- 1. Consulta que permita identificar los clientes que se encuentran en un departamento inhabilitado
+-- 2. Cuantos clientes hay en cada uno de los departamentos inhabilitados
+-- 3. Genere una consulta que muestre las edades aproximadas de los clientes (resta simple de año)
+-- 4. Genere una consulta que muestre los totales de clientes por rango de edad en segmentos de 20 años
+-- 5. Genere una consulta que muestre el limite de los clientes que se encuentran activos, que estan en un departamento habilitado, 
+-- y cuyo límite sea menor a $2000, con una columna que indique un aumento de límite segun las siguientes consideraciones: 
+-- Si es de San Salvador o la libertad, un aumento  del 15%, si es cualquier otro departamento del 10%
